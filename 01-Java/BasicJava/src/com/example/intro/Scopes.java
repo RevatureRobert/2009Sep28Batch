129b9bf1cc4fed3a55f1606cf61ca6cf75672cee
Irving Perez Santana <irvingperez@brandeis.edu>
2020-09-29 16:42:06 -0400

    break Work

--- 01-Java/BasicJava/src/com/example/intro/Scopes.java
+++ 01-Java/BasicJava/src/com/example/intro/Scopes.java
@@ -2,9 +2,16 @@ package com.example.intro;
 
 public class Scopes {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static String cat = "kitty"; //static scope
+	string cat1 = "cat1"; //instance scope
+	public static void main(String[] args) {  
+		boolean test = true; //method scope
 
+		if(test) {
+			int x = 4;   //block scope        
+		}
+		
+		System.out.println(x);
 	}
 
 }
