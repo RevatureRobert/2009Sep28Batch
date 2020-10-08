8367da1230e34d56c7989e10022f26de54d83ad5
connellrobert <robert.lel.connell@gmail.com>
2020-10-08 11:06:12 -0500

    adding jdbc

---
+++ 02-SQL/HelloJdbc/src/main/java/com/revature/repo/CircusDao.java
@@ -0,0 +1,39 @@
+package com.revature.repo;
+
+import java.util.List;
+
+import com.revature.model.Circus;
+
+public class CircusDao implements DaoContract<Circus, String>{
+
+	@Override
+	public List<Circus> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Circus findById(String i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Circus update(Circus t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Circus create(Circus t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(String i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
