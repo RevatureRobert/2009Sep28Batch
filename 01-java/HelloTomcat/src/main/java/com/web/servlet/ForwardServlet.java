360cc77310b9ee503d6f27f3456750c59d79c042
Justin Mayfield <jmmayfield349@gmail.com>
2020-10-15 09:55:45 -0400

    updating

--- 01-java/HelloTomcat/src/main/java/com/web/servlet/ForwardServlet.java
+++ 01-java/HelloTomcat/src/main/java/com/web/servlet/ForwardServlet.java
@@ -1,5 +1,33 @@
 package com.web.servlet;
 
-public class ForwardServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * 
+ * hierarchy of a servlet
+ * 
+ * 		servlet (interface)
+ * 			GenericServlet(AC
+ * 				HttpServlet(AC)
+ * 					CustomServlet (our class)
+ */
+
+public class ForwardServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		resp.sendRedirect("03-FRONT END/html/intro.html");
+		
+		req.getRequestDispatcher(path)
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
 }
