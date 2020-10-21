7f4990c2e497c2bf0ac05613c57d51a384ef1d04
connellrobert <robert.lel.connell@gmail.com>
2020-10-21 14:06:46 -0500

    day 3 of js

--- 04-JS/web/js/network.js
+++ 04-JS/web/js/network.js
@@ -3,3 +3,45 @@
     what does it stand for?
       Asynchronous Javascript and XML
 */
+
+function ajaxGetRequest(url, expression, method = "get") {
+  //step 1
+  const xhr = new XMLHttpRequest();
+
+  //step 2
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      /* 
+        status code
+          100 = informational
+          200 = ok
+          300 = redirect
+          400 = client side error
+          500 = server side error
+        
+        ready state
+          0 = request has not been initialized
+          1 = request has been initialized (the connection has been established)
+          2 = request has been received
+          3 = server is processing the request
+          4 = request has finished processing and the response is ready
+      */
+
+      const jsonResponse = JSON.parse(xhr.responseText);
+
+      expression(jsonResponse);
+    }
+  };
+
+  //step 3
+  xhr.open(method, url);
+
+  //step 4
+  xhr.send();
+}
+
+function fetchGetRequest(url, expression) {
+  return fetch(url)
+    .then((response) => response.json())
+    .then((result) => result);
+}
