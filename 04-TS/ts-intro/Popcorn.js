f5375e524bd08f805bbf220fc4b31375b18d8cdf
connellrobert <robert.lel.connell@gmail.com>
2020-10-22 15:47:09 -0500

    pushing ts stuff

---
+++ 04-TS/ts-intro/Popcorn.js
@@ -0,0 +1,5 @@
+export class Popcorn {
+    constructor() {
+        this.ripBag = () => console.log("fall on floor");
+    }
+}
---
+++ 04-TS/ts-intro/morePopcorn.ts
@@ -0,0 +1,11 @@
+import { Popcorn } from "./Popcorn";
+
+const kelseyPopcorn = new Popcorn();
+
+kelseyPopcorn.toppings.strange.push(["hot sauce", 3.4]);
+kelseyPopcorn.toppings.cheese.push(["parmesan", 4.2]);
+kelseyPopcorn.butterAmount = 2;
+kelseyPopcorn.salty = false;
+kelseyPopcorn.isBurnt = false;
+console.log(kelseyPopcorn);
+console.log(JSON.stringify(kelseyPopcorn));
